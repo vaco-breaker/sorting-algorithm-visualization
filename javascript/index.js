fa997ce2d64f76de177669847a8456c1cd8192d2
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-09-10 17:24:58 +0900

    feat: submit 버튼 클릭 시 최초 배열이 article 태그에 show up 되는 것 구현; formatting 적용

--- javascript/index.js
+++ javascript/index.js
@@ -3,7 +3,7 @@ const $sortOptions = $sortOptionBox.querySelectorAll('li');
 const $numberInput = document.querySelector('#numberInput');
 const $errorMessage = document.querySelector('#errorMessage');
 const $submitButton = document.querySelector('#submitButton');
-const $showSortingNumbers = document.querySelector("#showSortingNumbers");
+const $showSortingNumbers = document.querySelector('#showSortingNumbers');
 
 const SORT_OPTIONS = Object.freeze({
   BUBBLE: 'bubbleSort',
@@ -73,15 +73,15 @@ const pickSortingAlgorithmCallback = () => {
 
 const createInitialArray = (array) => {
   array.forEach((number, index) => {
-    const newElement = document.createElement("div");
+    const newElement = document.createElement('div');
     newElement.textContent = number;
-    newElement.classList.add("sortingArrayElement");
+    newElement.classList.add('sortingArrayElement');
     newElement.id = number;
     newElement.dataset.index = index;
 
     $showSortingNumbers.appendChild(newElement);
-  })
-}
+  });
+};
 
 $numberInput.addEventListener('input', changeNumberInput);
 $sortOptionBox.addEventListener('click', clickSortOptions);

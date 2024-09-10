b5dfe1956753b428fe3c015377101934933c04c1
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-09-10 16:08:17 +0900

    feat: html, css 기본 레이아웃 구현 및 input 태그 및 sort option box 이벤트 리스너 구현; prettier 적용

--- javascript/index.js
+++ javascript/index.js
@@ -1,46 +1,46 @@
-const $sortOptionBox = document.querySelector(".sort-option-box");
-const $sortOptions = $sortOptionBox.querySelectorAll("li");
-const $numberInput = document.querySelector("#numberInput");
-const $errorMessage = document.querySelector("#errorMessage");
+const $sortOptionBox = document.querySelector('.sort-option-box');
+const $sortOptions = $sortOptionBox.querySelectorAll('li');
+const $numberInput = document.querySelector('#numberInput');
+const $errorMessage = document.querySelector('#errorMessage');
 
 const changeNumberInput = (e) => {
   const regExp = /[0-9 ]/g;
 
   if (!regExp.test(e.target.value)) {
-    $errorMessage.textContent = "숫자와 띄어쓰기만 입력 가능합니다!";
+    $errorMessage.textContent = '숫자와 띄어쓰기만 입력 가능합니다!';
   } else {
-    $errorMessage.textContent = "";
+    $errorMessage.textContent = '';
   }
 
   e.target.value = e.target.value.replace(/[^0-9 ]/g, '');
-}
+};
 
 const clickSortOptions = (e) => {
-  $sortOptions.forEach(el => {
+  $sortOptions.forEach((el) => {
     if (el.textContent === e.target.textContent) {
-      el.classList.add("selected");
-    } else if (e.target.tagName === "LI") {
-      el.classList.remove("selected");
+      el.classList.add('selected');
+    } else if (e.target.tagName === 'LI') {
+      el.classList.remove('selected');
     }
-  })
+  });
 
   switch (e.target.id) {
-    case "bubbleSort":
-      console.log("Bubble Sort Clicked!");
+    case 'bubbleSort':
+      console.log('Bubble Sort Clicked!');
       break;
-    case "insertionSort":
-      console.log("Insertion Sort Clicked!");
+    case 'insertionSort':
+      console.log('Insertion Sort Clicked!');
       break;
-    case "mergeSort":
-      console.log("Merge Sort Clicked!");
+    case 'mergeSort':
+      console.log('Merge Sort Clicked!');
       break;
-    case "selectionSort":
-      console.log("Selection Sort Clicked!");
+    case 'selectionSort':
+      console.log('Selection Sort Clicked!');
       break;
     default:
       break;
   }
-}
+};
 
-$numberInput.addEventListener("input", changeNumberInput);
-$sortOptionBox.addEventListener("click", clickSortOptions);
+$numberInput.addEventListener('input', changeNumberInput);
+$sortOptionBox.addEventListener('click', clickSortOptions);

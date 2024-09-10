772281aa06429e72884a91fb71fca6ef6279269d
howyoujini <howyoujini@gmail.com>
2024-09-10 20:53:39 +0900

    feat: selection sorting 완료

--- javascript/sort/selection.js
+++ javascript/sort/selection.js
@@ -1,1 +1,18 @@
-// selection algorithm 구현
+export default function* selectionSortingAlgorithm(array) {
+  yield array;
+
+  for (let i = 0; i < array.length; i++) {
+    let lowestIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[lowestIndex]) {
+        lowestIndex = j;
+      }
+    }
+
+    if (lowestIndex !== i) {
+      [array[i], array[lowestIndex]] = [array[lowestIndex], array[i]];
+      yield array;
+    }
+  }
+}

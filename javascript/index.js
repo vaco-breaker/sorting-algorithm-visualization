5fd901765846d369c051d4efdc8f4f6ff4cef945
howyoujini <howyoujini@gmail.com>
2024-09-10 16:56:49 +0900

    feat: button onSubmit 구현 및 리팩토링

--- javascript/index.js
+++ javascript/index.js
@@ -2,6 +2,16 @@ const $sortOptionBox = document.querySelector('.sort-option-box');
 const $sortOptions = $sortOptionBox.querySelectorAll('li');
 const $numberInput = document.querySelector('#numberInput');
 const $errorMessage = document.querySelector('#errorMessage');
+const $submitButton = document.querySelector("#submitButton");
+
+const SORT_OPTIONS = Object.freeze({
+  BUBBLE: "bubbleSort",
+  INSERTION: "insertionSort", 
+  MERGE: "mergeSort",
+  SELECTION: "selectionSort"
+});
+
+let selectedSortOption = SORT_OPTIONS.BUBBLE;
 
 const changeNumberInput = (e) => {
   const regExp = /[0-9 ]/g;
@@ -11,36 +21,53 @@ const changeNumberInput = (e) => {
   } else {
     $errorMessage.textContent = '';
   }
-
-  e.target.value = e.target.value.replace(/[^0-9 ]/g, '');
+  console.log("before== ", e.target.value)
+  e.target.value = e.target.value.replace(/ +(?= )|[^0-9 ]/g, '');
+  console.log("after== ", e.target.value)
 };
 
 const clickSortOptions = (e) => {
   $sortOptions.forEach((el) => {
-    if (el.textContent === e.target.textContent) {
+    if (el.id === e.target.id) {
       el.classList.add('selected');
     } else if (e.target.tagName === 'LI') {
       el.classList.remove('selected');
     }
   });
 
-  switch (e.target.id) {
-    case 'bubbleSort':
+  selectedSortOption = e.target.id;
+};
+
+const pickSortingAlgorithm = (option, targetArray) => {
+  switch (option) {
+    case SORT_OPTIONS.BUBBLE:
       console.log('Bubble Sort Clicked!');
+      console.log(targetArray);
       break;
-    case 'insertionSort':
+    case SORT_OPTIONS.INSERTION:
       console.log('Insertion Sort Clicked!');
+      console.log(targetArray);
       break;
-    case 'mergeSort':
+    case SORT_OPTIONS.MERGE:
       console.log('Merge Sort Clicked!');
+      console.log(targetArray);
       break;
-    case 'selectionSort':
+    case SORT_OPTIONS.SELECTION:
       console.log('Selection Sort Clicked!');
+      console.log(targetArray);
       break;
     default:
       break;
   }
-};
+}
+
+const pickSortingAlgorithmCallback = () => {
+  const inputValue = $numberInput.value;
+  const numberArray = inputValue.split(' ').filter((value) => value !== '' && value !== ' ');
+
+  pickSortingAlgorithm(selectedSortOption, numberArray);
+}
 
 $numberInput.addEventListener('input', changeNumberInput);
 $sortOptionBox.addEventListener('click', clickSortOptions);
+$submitButton.addEventListener('click', pickSortingAlgorithmCallback);

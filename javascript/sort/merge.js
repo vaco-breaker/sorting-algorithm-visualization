6b0799c9c62848b71cb4abfb4b3572fcdeb5fa12
Seongnam-si <hongdev26@gmail.com>
2024-09-11 15:22:30 +0900

    feat: merge 병합 기본 알고리즘 작성완료, 애니메이션 구현 진행중

--- javascript/sort/merge.js
+++ javascript/sort/merge.js
@@ -1,1 +1,32 @@
-// merge algorithm 구현
+const mergeFuntion = (front, back) => {
+    
+    const resultArray = [];
+
+	while (front.length !== 0 && back.length !== 0) {
+		front[0] <= back[0] ? resultArray.push(front.shift()) : resultArray.push(back.shift());	
+	}
+
+    if (front.length === 0) resultArray.push(...back);
+    if (back.length === 0) resultArray.push(...front);
+    return resultArray;
+}
+
+
+
+export function* divideFuntion(array) {
+
+    if (array.length === 1) return array;
+
+    const pointIndex = Math.floor(array.length / 2);
+    const frontArray = array.slice(0, pointIndex);
+    const backArray = array.slice(pointIndex);
+    const newArray = [...frontArray, ...backArray];
+    yield newArray;
+    
+    
+
+    const result = mergeFuntion(Array.from(divideFuntion(frontArray)), Array.from(divideFuntion(backArray)));
+
+    return result;
+}
+

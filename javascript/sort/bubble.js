1de0311923aa337574a3f3864716c306ec3c61a6
Frogman113 <bainstud@naver.com>
2024-09-11 14:16:09 +0900

    config: bubble.js console.log remove # 2

--- javascript/sort/bubble.js
+++ javascript/sort/bubble.js
@@ -4,7 +4,6 @@ function* bubbleUp(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
-        console.log(arr[j], arr[j + 1]);
         const temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
